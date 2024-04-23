79690e794b6025d4b5bc16c6f1dfde704e4f9bc6
Kaj Ailomaa <kaj.ailomaa@gu.se>
2024-04-23 15:20:08 +0200

    removed some confusion from the create virtual camera script

--- create_virtual_camera_device.sh
+++ create_virtual_camera_device.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo modprobe v4l2loopback \
-        devices=2 exclusive_caps=1,1 video_nr=20 \
+        devices=1 exclusive_caps=1 video_nr=20 \
         card_label="v4l2 Virtual camera"\
         max_buffers=2
